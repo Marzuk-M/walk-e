f43c936539dd14b93d485f59932f5149a530caa2
Marzuk Mashrafe <marzukmashrafe2@gmail.com>
2022-10-15 17:55:51 -0400

    [1203171105151260] - Added React router and home, explore, rankings and account (initial) pages

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,11 +1,18 @@
 import Header from "./Header"
 import Footer from "./Footer"
+import CustomRouter from '../routes'
+// import { Routes, Route, Link } from "react-router-dom";
+// import {Home} from './pages/home'
 
 function App() {
   return <div>
     
     <Header/>
-    <h1>Body</h1>
+    {/* <h1>Bodys</h1> */}
+      <CustomRouter/>
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes> */}
     <Footer/>
 
   </div>;
