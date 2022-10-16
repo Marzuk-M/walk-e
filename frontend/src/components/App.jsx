ff435d503e86cf220ed6ceff3c5b8ff8571180c4
Marzuk Mashrafe <marzukmashrafe2@gmail.com>
2022-10-15 20:40:11 -0400

    Merge conflict resolved

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,26 +1,17 @@
 import Header from "./Header"
 import Footer from "./Footer"
-<<<<<<< HEAD
 import Login from "./Login"
-=======
 import CustomRouter from '../routes'
 // import { Routes, Route, Link } from "react-router-dom";
 // import {Home} from './pages/home'
->>>>>>> c340c2b7ce4151b98a706954acd9640e84b0ded3
 
 function App() {
   return (<div>
-    
-<<<<<<< HEAD
-    {/* <Header/>
-    <h1>Body</h1>
-    <Footer/> */}
-    <Login/>
-=======
+
+    {/* <Login/> */}
     <Header/>
       <CustomRouter/>
     <Footer/>
->>>>>>> c340c2b7ce4151b98a706954acd9640e84b0ded3
 
   </div>);
 }
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,6 +4,7 @@ import HomeIcon from "@mui/icons-material/Home";
 import ExploreIcon from "@mui/icons-material/Explore";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LeaderboardIcon from "@mui/icons-material/Leaderboard";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
@@ -16,30 +17,38 @@ function Footer() {
         className="bottomNavbar"
       >
         <div className="container">
-          <BottomNavigationAction
-            showLabel
-            sx={{ color: "whitesmoke" }}
-            label="Home"
-            icon={<HomeIcon />}
-          />
-          <BottomNavigationAction
-            showLabel
-            sx={{ color: "whitesmoke" }}
-            label="Explore"
-            icon={<ExploreIcon />}
-          />
-          <BottomNavigationAction
-            showLabel
-            sx={{ color: "whitesmoke" }}
-            label="Rankings"
-            icon={<LeaderboardIcon />}
-          />
-          <BottomNavigationAction
-            showLabel
-            sx={{ color: "whitesmoke" }}
-            label="Profile"
-            icon={<AccountCircleIcon />}
-          />
+          <Link to="/"> 
+            <BottomNavigationAction
+              showLabel
+              sx={{ color: "whitesmoke" }}
+              label="Home"
+              icon={<HomeIcon />}
+            />
+          </Link>
+          <Link to="/explore">
+            <BottomNavigationAction
+              showLabel
+              sx={{ color: "whitesmoke" }}
+              label="Explore"
+              icon={<ExploreIcon />}
+            />
+          </Link>
+          <Link to="rankings">
+            <BottomNavigationAction
+              showLabel
+              sx={{ color: "whitesmoke" }}
+              label="Rankings"
+              icon={<LeaderboardIcon />}
+            />
+          </Link>
+          <Link to="account">
+            <BottomNavigationAction
+              showLabel
+              sx={{ color: "whitesmoke" }}
+              label="Profile"
+              icon={<AccountCircleIcon />}
+            />
+          </Link>
         </div>
       </BottomNavigation>
     </Paper>
