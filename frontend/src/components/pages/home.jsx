25d59bbb0b62bc6810c7f6a90fd2a6619638f91b
Marzuk Mashrafe <marzukmashrafe2@gmail.com>
2022-10-16 06:16:27 -0400

    Updated Homepage

--- frontend/src/components/pages/home.jsx
+++ frontend/src/components/pages/home.jsx
@@ -1,9 +1,47 @@
+import { 
+  Card, Grid, Box, Container, List, 
+  ListItem, ListItemAvatar, ListItemText, 
+  Divider, Avatar, Typography, ListItemIcon
+} from "@mui/material";
 import React from "react";
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+
+const dto = {
+  title: "Brunch this weekend?",
+  body: "I'll be in your neighborhood doing errands this…",
+}
+const name = "John"
 
 export default function Home() {
+  const listItem = (data) => {
+    return (
+      <>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar><AlternateEmailIcon/></Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            primary={data.title}
+            secondary={data.body}
+          />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+      </>
+    )
+  }
+
   return (
-    <div>
-      Home
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Card variant="elevation" sx={{ width: '100%', height: '7em', marginTop: '1em' }}>
+        <Typography variant="h3" sx={{ p: 3 }}>
+          Hello, {name}!
+        </Typography>
+      </Card>
+      <Card variant="elevation" sx={{ width: '100%', marginTop: '1em', maxHeight: "calc(100% - 15em)", overflowY: 'auto' }}>
+        <List>
+          {[1,2,3,4,5,6,7].map(() => listItem(dto))}
+        </List>
+      </Card>
+    </Container>
   );
 }
